700b18cd7f233d8d0850c45a87159952b9e379b3
Muhammad Jalloh <dev@TechBuktu.com>
2018-01-19 16:16:34 -0500

    Started setup of AppRoutingModule

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+
+//Import all components needing routing
+import { HeroesComponent } from './components/heroes/heroes.component';
+
+export class AppRoutingModule()
+
