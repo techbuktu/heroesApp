514e3a5240f16bfc8abaa2505fb695b204a0cf26
Muhammad Jalloh <dev@TechBuktu.com>
2018-01-21 02:15:27 -0500

    Implemented HeroesComponent's child component and passed (via @Input() ) the 'selectedHero' property into.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,9 @@ import {Routes, RouterModule} from '@angular/router';
 
 //Import all components needing routing
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
 
-export class AppRoutingModule()
+export class AppRoutingModule{
+
+}
 
