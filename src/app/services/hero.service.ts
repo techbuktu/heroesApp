be706b5c607b7e1588489a4cfde14caae4e116f3
Muhammad Jalloh <dev@TechBuktu.com>
2018-01-22 00:17:30 -0500

    Created HeroService and implemented mock-data-based getHeroes() method

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
 
+//Import the Hero class and the mock heroes data
+import { Hero } from '../classes/hero';
+import { HEROES } from '../mock-data/heroes.data';
+
+
 @Injectable()
 export class HeroService {
 
+
   constructor() { }
+
+  getHeroes(): Hero[] {
+    return HEROES;
+  }
 
 }
