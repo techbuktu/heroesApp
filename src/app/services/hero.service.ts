f8329ea2cbcb0f899dd09f8e2bd9b20c079737a6
Muhammad Jalloh <dev@TechBuktu.com>
2018-01-22 18:16:08 -0500

    Updated add() and clear() methods of MessageService; added service to constructor of HeroService

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -3,12 +3,13 @@ import { Injectable } from '@angular/core';
 //Import the Hero class and the mock heroes data
 import { Hero } from '../classes/hero';
 import { HEROES } from '../mock-data/heroes.data';
+
+//Import the MessageService for us in the HeroService.
 import { MessageService } from './message.service';
 
 //Import RxJS Observable object to simulate returning data from a Http API Service
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
-
 
 
 @Injectable()
@@ -20,6 +21,8 @@ export class HeroService {
   }
 
   getHeroes(): Observable<Hero[]> {
+    //Todo: send the message __after__ fetching the heroes.
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 
