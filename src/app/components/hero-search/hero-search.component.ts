92e3f9772476227f3a7dd89dd4b4a562ad58c168
Muhammad Jalloh <dev@TechBuktu.com>
2018-01-27 18:22:37 -0500

    Implemented methods of HeroSearchComponent to search, return and listen for stream of observable API responses.

--- src/app/components/hero-search/hero-search.component.ts
+++ src/app/components/hero-search/hero-search.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 //Import the HeroService Injectable for use in queries/searches over the API
 import { HeroService } from '../../services/hero.service';
+import { Hero } from '../../classes/hero';
+
+//Import reactive extensions/elements from the rxjs library
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import { of } from 'rxjs/observable/of';
+
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-hero-search',
@@ -9,11 +17,29 @@ import { HeroService } from '../../services/hero.service';
 })
 export class HeroSearchComponent implements OnInit {
 
+  heroes$: Observable<Hero[]>;
+  private searchTerms = new Subject<string>();
+
   constructor(private heroServicee: HeroService) {
     
    }
 
-  ngOnInit() {
+  // Push a search term into the observable stream.
+  search(term:string):void {
+    this.searchTerms.next(term);
+  }
+
+
+  ngOnInit():void {
+    this.heroes$ = this.searchTerms.pipe(
+      //Wait for 300ms after each keystroke before considering the term
+      debounceTime(300),
+      // Ignore new term is same as the precious term.
+      distinctUntilChanged(),
+
+      // Swicth to the new search observable each time the term changes.
+      switchMap((term:string) => this.heroServicee.searchHeroes(term)),
+    );
   }
 
 }
